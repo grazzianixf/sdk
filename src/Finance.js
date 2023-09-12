995e740198c76db26a4932da27b8162c193c1ea4
Grazziani Xavier <grazzianixf@gmail.com>
2023-09-12 19:41:47 -0300

    primeira versão funcional da funcao de divisao com rateio

--- src/Finance.js
+++ src/Finance.js
@@ -1,17 +1,43 @@
-function dividirComRateio(dividend, divisor) {
-   const quotient = Math.floor(dividend / divisor);
-   const remainder = dividend % divisor;
+const Utils = require("./../src/Utils");
 
-   let result = []
+/**
+ * 
+ * @param {Number} dividend 
+ * @param {Number} divisor 
+ * @param {Number} decimalPlace 
+ * @returns An array that contains a set of items with dividend prorated among these items
+ */
+function divideWithProratedRemainder(dividend, divisor, decimalPlace = 2) {
 
-   for (let i = 0; i < divisor; i++) {
-      result.push({ rownum: i + 1, size: divisor, value: quotient})
-      
+   if (!divisor || divisor === 0) {
+      return
    }
 
-   return result;
+	let result = [];
+	let multiplier = Math.pow(10, decimalPlace);
+	let roundedDownValue = Math.floor((dividend / divisor) * multiplier) / multiplier;
+
+	for (let i = 0; i < divisor; i++) {
+		result.push({ rownum: i + 1, size: divisor, value: roundedDownValue });
+	}
+
+   let total = Utils.Array.sumField(result, "value", decimalPlace)
+
+   if ((Math.round(total * multiplier) / multiplier) != (Math.round(dividend * multiplier) / multiplier)) {
+      let remainder = Math.round(((Math.round(dividend * multiplier) / multiplier) - (Math.round(total * multiplier) / multiplier)) * multiplier) / multiplier
+      
+      let i = 0
+      let remainderOneUnit = 1 / multiplier
+      while (remainder > 0) {
+         result[i].value += remainderOneUnit
+         remainder -= remainderOneUnit
+         i++
+      }
+   }
+
+	return result;
 }
 
 module.exports = {
-   dividirComRateio
-}
+	divideWithProratedRemainder,
+};
