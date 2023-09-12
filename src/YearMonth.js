568b8a251d5b20e92d46543c4647e1445f80479e
Grazziani Xavier <grazzianixf@gmail.com>
2023-09-12 13:51:01 -0300

    Melhorando documentação

--- src/YearMonth.js
+++ src/YearMonth.js
@@ -13,15 +13,22 @@ module.exports = class YearMonth {
 		this.#month = month;
 	}
 
+	/**
+	 * Set YearMonth by a specified Date
+	 * @param {Date} date
+	 */
 	fromDate(date) {
 		if (date) {
 			this.#year = date.getFullYear();
 			this.#month = date.getMonth() + 1;
-		} else {
-			constructor(null, null);
 		}
 	}
 
+	/**
+	 * Move YearMonth to forward or back by integer increment
+	 * @param {Number} increment
+	 * @returns
+	 */
 	move(increment) {
 		if (increment) {
 			let date = new Date(this.#year, this.#month - 1, 1);
@@ -33,10 +40,16 @@ module.exports = class YearMonth {
 		return this;
 	}
 
+	/**
+	 * Move YearMonth to previous. Same that 'move(-1)'
+	 */
 	previous() {
 		this.move(-1);
 	}
 
+	/**
+	 * Move YearMonth to next. Same that 'move(1)'
+	 */
 	next() {
 		this.move(1);
 	}
@@ -53,12 +66,18 @@ module.exports = class YearMonth {
 		return this.#month;
 	}
 
+	/**
+	 * Get a string in format 'MonthName'
+	 */
 	get monthDescription() {
 		return new Date(this.#year, this.#month - 1).toLocaleDateString(LOCALE, {
 			month: "long",
 		});
 	}
 
+	/**
+	 * Get a string in format 'MonthName Year'
+	 */	
 	get description() {
 		return new Date(this.#year, this.#month - 1).toLocaleDateString(LOCALE, {
 			year: "numeric",
@@ -66,6 +85,9 @@ module.exports = class YearMonth {
 		});
 	}
 
+	/**
+	 * Get a object { year, month }
+	 */
 	get value() {
 		return { year: this.#year, month: this.#month };
 	}
