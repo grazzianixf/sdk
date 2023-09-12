51b01939b06593392d18e282a0f2d2ef4c23df5d
Grazziani Xavier <grazzianixf@gmail.com>
2023-09-12 13:25:41 -0300

    Melhoria na operação do YearMonth

--- src/YearMonth.js
+++ src/YearMonth.js
@@ -2,7 +2,7 @@ const Utils = require("./Utils");
 
 const MIN_MONTH = 1;
 const MAX_MONTH = 12;
-const LOCALE = 'en-us';
+const LOCALE = "en-us";
 
 module.exports = class YearMonth {
 	#year = null;
@@ -13,31 +13,35 @@ module.exports = class YearMonth {
 		this.#month = month;
 	}
 
-	#move(increment) {
-		// console.log("increment", increment)
+	fromDate(date) {
+		if (date) {
+			this.#year = date.getFullYear();
+			this.#month = date.getMonth() + 1;
+		} else {
+			constructor(null, null);
+		}
+	}
+
+	move(increment) {
+		if (increment) {
+			let date = new Date(this.#year, this.#month - 1, 1);
+			date.setMonth(date.getMonth() + increment);
+
+			this.fromDate(date);
+		}
+
+		return this;
 	}
 
 	previous() {
-		// this.#move(-1)
-		if (this.#month == MIN_MONTH) {
-			this.#month = MAX_MONTH;
-			this.#year = this.#year - 1;
-		} else {
-			this.#month = this.#month - 1;
-		}
+		this.move(-1);
 	}
 
 	next() {
-		// this.#move(1)
-		if (this.#month == MAX_MONTH) {
-			this.#month = MIN_MONTH;
-			this.#year = this.#year + 1;
-		} else {
-			this.#month = this.#month + 1;
-		}
+		this.move(1);
 	}
 
-	toString(){
+	toString() {
 		return Utils.toString(this);
 	}
 
@@ -50,10 +54,19 @@ module.exports = class YearMonth {
 	}
 
 	get monthDescription() {
-		return new Date(this.#year,this.#month-1).toLocaleDateString(LOCALE, { month:"long"})
-   }
-   
-   get description() {
-      return new Date(this.#year,this.#month-1).toLocaleDateString(LOCALE, { year:"numeric", month:"long"})
-   }
+		return new Date(this.#year, this.#month - 1).toLocaleDateString(LOCALE, {
+			month: "long",
+		});
+	}
+
+	get description() {
+		return new Date(this.#year, this.#month - 1).toLocaleDateString(LOCALE, {
+			year: "numeric",
+			month: "long",
+		});
+	}
+
+	get value() {
+		return { year: this.#year, month: this.#month };
+	}
 };
