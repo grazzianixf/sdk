da1eb288413b03c88263034dc5b271272d005baa
Grazziani Xavier <grazzianixf@gmail.com>
2023-09-15 19:42:58 -0300

    teste para implementar um FirebaseHelper

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const Test = require("./Test")
 const Utils = require("./Utils");
 const YearMonth = require("./YearMonth");
 
+const FirebaseHelper = require("./FirebaseHelper")
+
 module.exports = { 
    Finance, 
+   FirebaseHelper,
    Test,
    Utils, 
    YearMonth 
