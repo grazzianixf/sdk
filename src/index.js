d359db2fe4864646de6f420a2c82289c37554b91
Grazziani Xavier <grazziani.xavier@dataprev.gov.br>
2024-01-30 16:28:43 -0300

    bkp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const Finance = require("./Finance");
 const Test = require("./Test")
-const User = require("./User")
 const Utils = require("./Utils");
 const YearMonth = require("./YearMonth");
 
 module.exports = { 
    Finance, 
    Test,
-   User,
    Utils, 
    YearMonth 
 };
